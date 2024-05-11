93ff1cfac91c8696641c744eaeb138c7bd162ea5
Org0220 <73370436+Org0220@users.noreply.github.com>
2024-05-11 17:03:09 -0400

    php conversion
    php conversion

---
+++ FrontEnd/index.php
@@ -0,0 +1,24 @@
+<?php include_once("include/header.php"); ?>
+
+<body>
+
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">Phishing Academy</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="quiz.php">Quiz</a></li>
+        <li><a href="phishing_checker.php">Phishing Checker</a></li>
+        <li><a href="bot.php">Chat Bot</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    <h3>Basic Navbar Example</h3>
+    <p id="par">A navigation bar is a navigation header that is placed at the top of the page.</p>
+  </div>
+
+  <?php include_once("include/footer.php"); ?>
---
+++ FrontEnd/quiz.php
@@ -0,0 +1,36 @@
+<?php include_once("include/header.php"); ?>
+
+
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Phishing Academy</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="index.html">Home</a></li>
+      <li><a href="quiz.html">Quiz</a></li>
+      <li><a href="phishingcheker.html">Phishing Checker</a></li>
+      <li><a href="bot.html">Chat Bot</a></li>
+    </ul>
+  </div>
+</nav>
+<div class="container">
+  <h3>Quiz</h3>
+  <p>Take this quiz to test your knowledge on phishing.</p>
+</div>
+
+<div class="container">
+  <div class="panel panel-default">
+    <div class="panel-body">
+
+      <div class="btn-group">
+        <button type="button" class="btn btn-primary">Left</button>
+        <button type="button" class="btn btn-primary">Middle</button>
+        <button type="button" class="btn btn-primary">Right</button>
+      </div>
+    </div>
+  </div>
+
+
+  <?php include_once("include/footer.php"); ?>
