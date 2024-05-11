9bbf69d81ba890b3830e2e4178d0571ad3a2702c
Hrag Bankian <hrag.bankian@gmail.com>
2024-05-11 15:05:28 -0400

    Added dataset files

---
+++ BackEnd/dataset.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import classification_report
+
+df = pd.read_csv('spam.csv', encoding='latin-1')
+df.head()
