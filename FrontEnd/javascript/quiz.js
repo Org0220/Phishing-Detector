4ecc7abb097e5636a22a38946a89b5ea9725e03b
Org0220 <73370436+Org0220@users.noreply.github.com>
2024-05-11 16:55:31 -0400

    fetch data first
    fetch data first

--- FrontEnd/javascript/quiz.js
+++ FrontEnd/javascript/quiz.js
@@ -1,20 +1,48 @@
 
 
 
+
 //parse emails from data/mixed_email.json file
 
-fetch('data/mixed_email.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
+fetch('../FrontEnd/data/mixed_emails.json')
+    .then(response => response.json())
+    .then(data => {
+        let emails = data.map(email => {
+            let emailBody = email['body']
+            let emailSubject = email['subject']
+            let emailSender = email['sender']
+            let emailRecipients = email['recipients']
+            let emailDate = email['date']
+            let emailId = email['id']
+            let emailElement = {
+                emailBody,
+                emailSubject,
+                emailSender,
+                emailRecipients,
+                emailDate,
+                emailId
+            }
+            return emailElement
+        })
+
+        // create an email component for each email
+        emails.forEach(email => {
+            let emailComponent = document.createElement('div')
+            emailComponent.className = 'email'
+            emailComponent.innerHTML = `
+                    <div class="email-sender">${email.emailSender}</div>
+                    <div class="email-subject">${email.emailSubject}</div>
+                    <div class="email-body
+                    ">${email.emailBody}</div>
+                    <div class="email-recipients">${email.emailRecipients}</div>
+                    <div class="email-date
+                    ">${email.emailDate}</div>
+                    <div class="email-id">${email.emailId}</div>
+                `
+            document.body.appendChild(emailComponent)
+        })
     })
-    .then(jsonArray => {
-        // Now you have jsonArray as a JavaScript array
-        console.log(jsonArray);
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-    });
+    .catch(error => console.error(error))
+
+
 
