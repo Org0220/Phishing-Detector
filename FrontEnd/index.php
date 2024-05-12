b578926ce399ffc9b729a6d8ba1a8f12368773b0
Gassia04 <137027254+Gassia04@users.noreply.github.com>
2024-05-11 20:48:45 -0400

    deleting navbara from index
    deleting navbara from index

--- FrontEnd/index.php
+++ FrontEnd/index.php
@@ -1,20 +1,6 @@
 <?php include_once("include/header.php"); ?>
 
-<body>
 
-  <nav class="navbar navbar-default">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="#">Phishing Academy</a>
-      </div>
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="index.php">Home</a></li>
-        <li><a href="quiz.php">Quiz</a></li>
-        <li><a href="phishing_checker.php">Phishing Checker</a></li>
-        <li><a href="bot.php">Chat Bot</a></li>
-      </ul>
-    </div>
-  </nav>
 
   <div class="container">
     <h3>Basic Navbar Example</h3>
